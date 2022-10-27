e94b1400cfdda77023b0fff1e127a00eac5fced6
Hackintosh-VM <l.yang.ze.s@gmail.com>
2022-10-28 06:00:08 +0800

    test

--- motor_control/setup.py
+++ motor_control/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-package_name = 'xbox_controller'
+package_name = 'motor_control'
 
 setup(
     name=package_name,
@@ -13,14 +13,13 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='pi',
+    maintainer='root',
     maintainer_email='l.yang.ze.s@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'read_controller    = xbox_controller.controller:main',
         ],
     },
 )

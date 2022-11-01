07a8cb48a56887289460c00715c07bcfaf2f0855
Lya-M1RA <l.yang.ze.s@gmail.com>
2022-11-01 14:28:35 +0800

    modified:   src/motor_control/motor_control/local_controller.py

--- src/motor_control/motor_control/local_controller.py
+++ src/motor_control/motor_control/local_controller.py
@@ -12,6 +12,13 @@ class LocalController(Node):
     def __init__(self,name):
         super().__init__(name)
 
+        gear = [False, 0]
+        emerg_stop = False
+        parking_mode = [False, False]
+        orientation = 0
+        throttle = 0
+        brake = 0
+
         self.sub_joy = self.create_subscription(
             Joy, 
             "/joy", 
@@ -23,31 +30,30 @@ class LocalController(Node):
             '/send_can0',
             10)
 
-        self.declare_parameter('gear', 0)
-        self.declare_parameter('park', True)
-
 
     def joy_recv(self,msg):
-        left_joy_x = msg.axes[0]
-        left_joy_y = msg.axes[1]
-        right_joy_x = msg.axes[2]
-        right_joy_y = msg.axes[3]
-        dpad_x = msg.axes[6]
-        dpad_y = msg.axes[7]
-        left_trigger = msg.axes[4]
-        right_trigger = msg.axes[5]
-        left_shoulder_button = msg.buttons[4]
-        right_shoulder_button = msg.buttons[5]
-        a_button = msg.buttons[0]
-        b_button = msg.buttons[1]     
-        x_button = msg.buttons[2]
-        y_button = msg.buttons[3]
-        select_button = msg.buttons[6]
-        start_button = msg.buttons[7]
-        xbox_button = msg.buttons[8]
-        left_joy_button = msg.button[9]
-        right_joy_button = msg.button[10]
-        self.input_processor()
+        input = {
+            'left_joy_x'            : msg.axes[0]     ,
+            'left_joy_y'            : msg.axes[1]     ,
+            'right_joy_x'           : msg.axes[2]     ,
+            'right_joy_y'           : msg.axes[3]     ,
+            'dpad_x'                : msg.axes[6]     ,
+            'dpad_y'                : msg.axes[7]     ,
+            'left_trigger'          : msg.axes[4]     ,
+            'right_trigger'         : msg.axes[5]     ,
+            'left_shoulder_button'  : msg.buttons[4]  ,
+            'right_shoulder_button' : msg.buttons[5]  ,
+            'a_button'              : msg.buttons[0]  ,
+            'b_button'              : msg.buttons[1]  ,   
+            'x_button'              : msg.buttons[2]  ,
+            'y_button'              : msg.buttons[3]  ,
+            'select_button'         : msg.buttons[6]  ,
+            'start_button'          : msg.buttons[7]  ,
+            'xbox_button'           : msg.buttons[8]  ,
+            'left_joy_button'       : msg.button[9]   ,
+            'right_joy_button'      : msg.button[10]  
+        }
+        self.input_processor(input)
 
 
     def can_send(self, left_frame_data, right_frame_data):
@@ -57,8 +63,8 @@ class LocalController(Node):
         self.pub_can.publish(msg)
 
 
-    def input_processor(self):
-        
+    def input_processor(self, input):
+        if input['b_button'] == 
         
 
 

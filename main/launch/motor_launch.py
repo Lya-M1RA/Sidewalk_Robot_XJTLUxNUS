83dacb0fab4e5ebd6dfae1d434008ec9391a308d
Mira-Laptop <l.yang.ze.s@gmail.com>
2023-02-22 12:57:42 +0800

    modified:   main/launch/motor_launch.py

--- main/launch/motor_launch.py
+++ main/launch/motor_launch.py
@@ -5,21 +5,25 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='can_bus',
+            namespace='send_can0',
             executable='send_can0',
             name='send_can0'
         ),
         Node(
             package='can_bus',
+            namespace='recv_can0',
             executable='recv_can0',
             name='recv_can0'
         ),
         Node(
             package='can_bus',
+            namespace='encoder_can0',
             executable='encoder_can0',
             name='encoder_can0'
         ),
         Node(
             package='motor_control',
+            namespace='local_controller',
             executable='local_controller',
             name='local_controller'
         ),

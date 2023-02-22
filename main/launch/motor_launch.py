ae440da73d1aa5667ae7dbd1c6cb8e60e6f1e305
Mira-Laptop <l.yang.ze.s@gmail.com>
2023-02-22 12:21:45 +0800

    new file:   main/launch/motor_launch.py

---
+++ main/launch/motor_launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='can_bus',
+            executable='send_can0',
+        )
+        Node(
+            package='can_bus',
+            executable='recv_can0',
+        )
+        Node(
+            package='can_bus',
+            executable='encoder_can0',
+        )
+        Node(
+            package='motor_control',
+            executable='local_controller',
+        )
+        Node(
+            package='joy',
+            executable='joy_node',
+        )
+    ])

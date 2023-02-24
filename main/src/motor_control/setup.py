17e9c6414bf2d0d8326aef30a6a8403b188b03e7
Mira-Laptop <l.yang.ze.s@gmail.com>
2023-02-25 06:28:46 +0800

    modified:   main/src/motor_control/setup.py

--- main/src/motor_control/setup.py
+++ main/src/motor_control/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'local_controller           = motor_control.local_controller:main'
+            'local_controller           = motor_control.local_controller:main',
             'encoder_counter            = motor_control.encoder_counter:main'
         ],
     },

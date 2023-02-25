7e4e91c4934537cdea68561948cfcae04534449b
Mira-Laptop <l.yang.ze.s@gmail.com>
2023-02-25 13:04:04 +0800

    modified:   main/launch/motor_launch.py 	modified:   main/src/can_bus/can_bus/can0_encoder.py 	modified:   main/src/can_bus/can_bus/can0_rx.py 	modified:   main/src/can_bus/setup.py 	modified:   main/src/motor_control/motor_control/encoder_counter.py 	modified:   main/src/motor_control/setup.py

--- main/launch/motor_launch.py
+++ main/launch/motor_launch.py
@@ -5,25 +5,21 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='can_bus',
-            namespace='send_can0',
             executable='send_can0',
             name='send_can0'
         ),
-        # Node(
-        #     package='can_bus',
-        #     namespace='recv_can0',
-        #     executable='recv_can0',
-        #     name='recv_can0'
-        # ),
-        # Node(
-        #     package='can_bus',
-        #     namespace='encoder_can0',
-        #     executable='encoder_can0',
-        #     name='encoder_can0'
-        # ),
+        Node(
+            package='can_bus',
+            executable='recv_can0',
+            name='recv_can0'
+        ),
+        Node(
+            package='can_bus',
+            executable='encoder_can0',
+            name='encoder_can0'
+        ),
         Node(
             package='motor_control',
-            namespace='local_controller',
             executable='local_controller',
             name='local_controller'
         ),
@@ -31,5 +27,10 @@ def generate_launch_description():
             package='joy',
             executable='joy_node',
             name='joy_node'
-        )
+        ),
+        Node(
+            package='motor_control',
+            executable='encoder_diff',
+            name='encoder_diff'
+        ),
     ])

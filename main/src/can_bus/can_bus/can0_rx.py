47faeea068b4efc37a0292b7734af133bc3385e6
Mira-Laptop <l.yang.ze.s@gmail.com>
2023-02-25 09:46:41 +0800

    modified:   main/src/can_bus/can_bus/can0_rx.py

--- main/src/can_bus/can_bus/can0_rx.py
+++ main/src/can_bus/can_bus/can0_rx.py
@@ -28,17 +28,15 @@ class Rx(Node):
     def timer_callback(self):
         can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan')
         message = can0.recv(0)
-        self.get_logger().info("True")
         msg_l = RecvCAN0l()
         msg_r = RecvCAN0r()
-        if message != None:
-            # self.get_logger().info('True')
-            if (message.arbitration_id == 0x582) & (message.data[0:3] == [0x43,0x05,0x21]):
-                msg_l.left_motor_stat = message.data
-                self.recv_can_l.publish(msg_l)
-            if (message.arbitration_id == 0x583) & (message.data[0:3] == [0x43,0x05,0x21]):
-                msg_r.right_motor_stat = message.data
-                self.recv_can_r.publish(msg_r)
+        # if message != None:
+        if (message.arbitration_id == 0x582) & (message.data[0:3] == [0x43,0x05,0x21]):
+            msg_l.left_motor_stat = message.data
+            self.recv_can_l.publish(msg_l)
+        if (message.arbitration_id == 0x583) & (message.data[0:3] == [0x43,0x05,0x21]):
+            msg_r.right_motor_stat = message.data
+            self.recv_can_r.publish(msg_r)
 
 
 def main(args=None):

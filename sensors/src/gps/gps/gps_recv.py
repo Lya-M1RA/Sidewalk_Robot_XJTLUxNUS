e5b195f9041efe0a121c5fd819fe210321a6ad9b
Mira <l.yang.ze.s@gmail.com>
2023-03-02 20:40:30 +0800

    modified:   main/src/can_bus/can_bus/can0_encoder.py 	modified:   sensors/src/gps/gps/gps_recv.py

--- sensors/src/gps/gps/gps_recv.py
+++ sensors/src/gps/gps/gps_recv.py
@@ -1,21 +1,23 @@
-from socket import *
-import time
-import rclpy                                     # ROS2 Python Client Library
-from rclpy.node import Node   
+# from socket import *
+# import time
+# import rclpy                                     # ROS2 Python Client Library
+# from rclpy.node import Node   
 
-class GPS(Node):
+# class GPS(Node):
 
-    def __init__(self,name):
-        super().__init__(name)
+#     def __init__(self,name):
+#         super().__init__(name)
 
-        self.pub_can = self.create_publisher(
-            GPS,
-            'gps',
-            5)
+#         self.pub_can = self.create_publisher(
+#             GPS,
+#             'gps',
+#             5)
+        
 
-def main(args=None):
-    rclpy.init(args=args)
-    node = GPS('gps_node')
-    rclpy.spin(node)
-    node.destory_node()
-    rclpy.shutdown()
+
+# def main(args=None):
+#     rclpy.init(args=args)
+#     node = GPS('gps_node')
+#     rclpy.spin(node)
+#     node.destory_node()
+#     rclpy.shutdown()

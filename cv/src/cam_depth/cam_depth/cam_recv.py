89fe52e4e99cbb779436208003b574454da5f870
Mira-Ubuntu-Laptop <l.yang.ze.s@gmail.com>
2023-03-06 02:56:16 +0800

    pytorch added

--- cv/src/cam_depth/cam_depth/cam_recv.py
+++ cv/src/cam_depth/cam_depth/cam_recv.py
@@ -24,7 +24,7 @@ class ImageSubscriber(Node):
 
 def main(args=None):                            # ROS2节点主入口main函数
     rclpy.init(args=args)                       # ROS2 Python接口初始化
-    node = ImageSubscriber("cam_recv")  # 创建ROS2节点对象并进行初始化
+    node = ImageSubscriber("cam_recv")          # 创建ROS2节点对象并进行初始化
     rclpy.spin(node)                            # 循环等待ROS2退出
     node.destroy_node()                         # 销毁节点对象
     rclpy.shutdown()                            # 关闭ROS2 Python接口
